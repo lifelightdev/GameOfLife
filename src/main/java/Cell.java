5006c01e90d72889a4db561b489508b74de429df
Life Light <lifelightdev@gmail.com>
2025-04-09 14:35:21 +0200

    Add 2 tests and change Java version

---
+++ src/main/java/Cell.java
@@ -0,0 +1,1 @@
+public record Cell(int x, int y) {}
--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
-    public World nextGeneration() {
-        return null;
+
+    List<Cell> cellsAliveInTheWorld = new ArrayList<>();
+
+    public List<Cell> nextGeneration() {
+        List<Cell> cellsAliveInTheNextGeneration = new ArrayList<>();
+        if (cellsAliveInTheWorld.size() == 3) {
+            cellsAliveInTheNextGeneration.add(new Cell(1,1));
+        }
+        return cellsAliveInTheNextGeneration;
+    }
+
+    public void addCell(Cell cell) {
+        cellsAliveInTheWorld.add(cell);
     }
 }
